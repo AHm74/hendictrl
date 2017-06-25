d1a45623eec2b9301bf6e06d96ecbb590ebffb9c
AHm74 <anders@holmstrom.nu>
2017-06-25 16:36:57 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -67,7 +67,7 @@ class PID(object):
         if (self.yk < self.GMA_LLIM):
             self.yk = self.GMA_LLIM
 
-        return self.yk
+        return round(self.yk, 1)
 
 @cbpi.controller
 class PIDHendi(KettleController):
@@ -170,3 +170,91 @@ class HendiControl(ActorBase):
         self.pwm.ChangeDutyCycle(0)
         GPIO.output(int(self.onoff_pin), 0)
 
+@cbpi.step
+class ToBoilStep(StepBase):
+    '''
+    Just put the decorator @cbpi.step on top of a method
+    '''
+    # Properties
+    temp = Property.Number("Power", configurable=True)
+    kettle = StepProperty.Kettle("Kettle")
+    s = False
+
+    def init(self):
+        '''
+        Initialize Step. This method is called once at the beginning of the step
+        :return:
+        '''
+        # set target tep
+        self.s = False
+        print("ToBoilStep init: {}".format(int(self.temp)))
+        self.set_target_temp(self.temp, self.kettle)
+
+    def reset(self):
+        self.stop_timer()
+        self.set_target_temp(self.temp, self.kettle)
+
+    def finish(self):
+        self.set_target_temp(0, self.kettle)
+
+    def execute(self):
+        '''
+        This method is execute in an interval
+        :return:
+        '''
+
+        # Check if Target Temp is reached
+        #if self.get_kettle_temp(self.kettle) >= int(self.temp) and self.s is False:
+        #    self.s = True
+        #    self.notify("Step Temp Reached!", "Please press the next button to continue", timeout=None)
+        #self.actor_power(self.get_target_temp)
+        pass
+
+@cbpi.step
+class BoilStep(StepBase):
+    '''
+    Just put the decorator @cbpi.step on top of a method
+    '''
+    # Properties
+    temp = Property.Number("Power", configurable=True)
+    kettle = StepProperty.Kettle("Kettle")
+    timer = Property.Number("Timer in Minutes", configurable=True)
+
+    def init(self):
+        '''
+        Initialize Step. This method is called once at the beginning of the step
+        :return:
+        '''
+        # set target tep
+        self.set_target_temp(self.temp, self.kettle)
+
+    @cbpi.action("Start Timer Now")
+    def start(self):
+        '''
+        Custom Action which can be execute form the brewing dashboard.
+        All method with decorator @cbpi.action("YOUR CUSTOM NAME") will be available in the user interface
+        :return:
+        '''
+        if self.is_timer_finished() is None:
+            self.start_timer(int(self.timer) * 60)
+
+    def reset(self):
+        self.stop_timer()
+        self.set_target_temp(self.temp, self.kettle)
+
+    def finish(self):
+        self.set_target_temp(0, self.kettle)
+
+    def execute(self):
+        '''
+        This method is execute in an interval
+        :return:
+        '''
+
+        # Check if Timer is Running
+        if self.is_timer_finished() is None:
+            self.start_timer(int(self.timer) * 60)
+
+        # Check if timer finished and go to next step
+        if self.is_timer_finished() == True:
+            self.next()
